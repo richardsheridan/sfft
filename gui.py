a4c61c24e26037881e0bd1f56ad512ad14424217
Richard Sheridan <richard.sheridan@gmail.com>
2016-10-21 16:58:46 -0400

    first steps to gui base class
    eventually it will be a backend agnostic API

---
+++ gui.py
@@ -0,0 +1,99 @@
+from collections import OrderedDict
+from time import perf_counter
+
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Button, Slider
+
+
+@property
+def NotImplementedAttribute(self):
+    raise NotImplementedError
+
+
+def _force_cooldown(callback, cooling_object):
+    def cooldown_wrapper(val):
+        t = perf_counter()
+        if t - cooling_object.timestamp <= cooling_object.cooldown:
+            return
+        else:
+            cooling_object.timestamp = t
+            return callback(val)
+
+    return cooldown_wrapper
+
+
+def _force_int(callback, setter):
+    def int_wrapper(val):
+        if int(val) != val:
+            setter(int(val))
+            return
+        val = int(val)
+        callback(val)
+
+    return int_wrapper
+
+
+class MPLGUI:
+    cooldown = .1
+
+    def __init__(self):
+        self.fig = NotImplementedAttribute
+        self.axes = {}
+        self.artists = {}
+        self.buttons = {}
+        self.sliders = {}
+        self._parameter_sliders = []
+        self.slider_coords = NotImplementedAttribute
+        self.timestamp = perf_counter()
+
+        self.create_layout()
+
+    def show(self, block=True):
+        self.load_frame()
+        self.recalculate_vision()
+        self.refresh_plot()
+
+        if block:
+            plt.ioff()
+        else:
+            plt.ion()
+
+        plt.show()
+
+    def register_slider(self, name, callback, isparameter=True, forceint=False, **slider_kwargs):
+        if 'label' not in slider_kwargs:
+            slider_kwargs['label'] = name
+        ax = self.axes[name] = self.fig.add_axes(self.slider_coords)
+        sl = self.sliders[name] = Slider(ax, **slider_kwargs)
+        callback = _force_cooldown(callback)
+        if forceint:
+            callback = _force_int(callback, sl.set_val)
+        sl.on_changed(callback)
+        self.slider_coords[1] -= self.slider_coords[-1] * (5 / 3)
+
+    def register_button(self, name, callback, coords, widget=Button, **button_kwargs):
+        if 'label' not in button_kwargs:
+            button_kwargs['label'] = name
+        ax = self.axes[name] = self.fig.add_axes(coords)
+        sl = self.sliders[name] = Slider(ax, **button_kwargs)
+        sl.on_changed(callback)
+
+    def register_parameter(self, name):
+        self._parameter_sliders.append(self.sliders[name])
+
+    @property
+    def parameters(self):
+        sl = self.sliders
+        return OrderedDict((name, sl[name].val) for name in self._parameter_sliders)
+
+    def create_layout(self):
+        raise NotImplementedError
+
+    def load_frame(self):
+        raise NotImplementedError
+
+    def recalculate_vision(self):
+        raise NotImplementedError
+
+    def refresh_plot(self):
+        raise NotImplementedError
