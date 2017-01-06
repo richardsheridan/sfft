c4cbf4f9a96fe10554ae2602838a584e58cff102
Richard Sheridan <richard.sheridan@gmail.com>
2017-01-06 16:28:29 -0500

    fix centroid sorting method and locations

--- break_analyzer.py
+++ break_analyzer.py
@@ -24,7 +24,7 @@ def find_fiber_sides(break_image, left_edge, right_edge):
     profile = (np.sum(break_image[:, :left_x], axis=1, dtype=np.uint64) +
                np.sum(break_image[:, right_x + 1:], axis=1, dtype=np.uint64))
 
-    y_mid = (left_y + right_y + .5) // 2
+    y_mid = int(((left_y + right_y ) / 2.0)+0.5)
     top_side = np.argmin(profile[:y_mid])
     bottom_side = np.argmin(profile[y_mid:]) + y_mid
 
@@ -34,6 +34,7 @@ def find_fiber_sides(break_image, left_edge, right_edge):
 def select_break_image(image, break_centroid, width=.0062371):
     y, x = break_centroid
     rows, cols = image.shape
+    width = int(rows * width)
     r, c = int(y * (rows - 1)), int(x * (cols - 1))
     r0, r1 = max(r - width, 0), min(r + width + 1, rows)
     c0, c1 = max(c - width, 0), min(c + width + 1, cols)
@@ -47,8 +48,9 @@ def analyze_breaks(directory, stabilize_args=()):
         image_path = path.join(directory, )
         from fiber_locator import load_stab_img
         image = load_stab_img(image_path, *stabilize_args)
-        break_y, break_x = sorted(break_y, key=break_x.__getitem__), sorted(break_x)
-        for break_centroid in zip(break_y, break_x):
+        break_y, break_x = np.array(break_y), np.array(break_x)
+        sortind = np.argsort(break_x)
+        for break_centroid in zip(break_y[sortind], break_x[sortind]):
             break_image = select_break_image(image, break_centroid)
             left_edge, right_edge = find_gap_edges(break_image)
             top_side, bottom_side = find_fiber_sides(break_image, left_edge, right_edge)
