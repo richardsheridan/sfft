7aa6f72f830b72a4775a8acc294664f7bd10be61
Richard Sheridan <richard.sheridan@gmail.com>
2017-05-05 17:50:34 -0400

    improve break analyzer by finding fiber sides first
     More robust. Also improve code structure!

--- break_analyzer.py
+++ break_analyzer.py
@@ -15,23 +15,39 @@ def find_gap_edges(break_image):
     return left_edge, right_edge
 
 
-def find_fiber_sides(break_image, left_x, right_x):
-    profile = (np.sum(break_image[:, :round(left_x)], axis=1, dtype=np.int64) +
-               np.sum(break_image[:, round(right_x) + 1:], axis=1, dtype=np.int64))
+def find_fiber_sides(break_image):
+    profile = np.sum(break_image, axis=1, dtype=np.int64)
 
     crossings = find_zero_crossings(np.gradient(profile), 'upward')
     minima_values = profile[crossings]
     minima_indices = np.where(crossings)[0]
+
     sorted_minima_indices = minima_indices[np.argsort(minima_values)]
-    top_side, bottom_side = sorted_minima_indices[:2]
+    top_side, bottom_side = sorted(sorted_minima_indices[:2])
+
+    # TODO this seems better in principle but can crash, fix it up?
+    # i = np.argmin(minima_values)
+    # j = i+1 if minima_values[i+1] < minima_values[i-1] else i-1
+    # top_side, bottom_side = sorted(minima_indices[[i,j]])
+
+    # import matplotlib.pyplot as plt
+    # a=plt.subplot(2,1,1,)
+    # plt.cla()
+    # plt.imshow(np.rot90(break_image,1), aspect='auto')
+    # plt.subplot(2,1,2, sharex=a)
+    # plt.cla()
+    # plt.plot(profile)
+    # plt.plot(minima_indices,minima_values,'rx')
+    # plt.plot([top_side,bottom_side],profile[[top_side,bottom_side]],'bo')
+    # plt.show(0)
+    # plt.pause(.0001)
 
     top_side = quadratic_subpixel_extremum_1d(profile, top_side)
     bottom_side = quadratic_subpixel_extremum_1d(profile, bottom_side)
-
     return top_side, bottom_side
 
 
-def select_break_image(image, break_centroid, width=.0062371):
+def select_break_image(image, break_centroid, width=0.00311855):
     y, x = break_centroid
     rows, cols = image.shape
     width = int(cols * width)
@@ -42,37 +58,57 @@ def select_break_image(image, break_centroid, width=.0062371):
     return break_image
 
 
+def analyze_break(break_image, verbose=False, animate=False):
+    top_side, bottom_side = find_fiber_sides(break_image)
+    b = int(round(bottom_side))
+    t = int(round(top_side))
+    (left_edge_y, left_edge), (right_edge_y, right_edge) = find_gap_edges(break_image[t:b, :])
+    gap_width = right_edge - left_edge
+    fiber_diameter = bottom_side - top_side
+
+    if verbose:
+        print('Top side: ', top_side)
+        print('Bottom side: ', bottom_side)
+        print('Left edge: ', left_edge)
+        print('Right edge: ', right_edge)
+        print('Gap width: ', gap_width * PIXEL_SIZE_X, ' um')
+        print('Fiber diameter', fiber_diameter * PIXEL_SIZE_X, ' um')
+
+    if animate:
+        import matplotlib.pyplot as plt
+        plt.cla()
+        plt.imshow(break_image, cmap='gray')
+        plt.plot([left_edge, right_edge], [left_edge_y + top_side, right_edge_y + top_side], 'rx')
+        plt.axvline(left_edge, linestyle=':', color='red')
+        plt.axvline(right_edge, linestyle=':', color='red')
+        plt.axhline(top_side, linestyle=':', color='green')
+        plt.axhline(bottom_side, linestyle=':', color='green')
+        plt.show(0)
+        plt.pause(0.00001)
+
+    return gap_width, fiber_diameter
+
+
+def sorted_centroids(break_y, break_x):
+    break_y, break_x = np.array(break_y), np.array(break_x)
+    sortind = np.argsort(break_x)
+    return list(zip(break_y[sortind], break_x[sortind]))
+
+
 def analyze_breaks(directory, stabilize_args=()):
-    names_breaks = load_breaks(directory, 'absolute')
-    for name, (break_y, break_x) in names_breaks:
+    for name, (break_y, break_x) in load_breaks(directory, 'absolute'):
         print('\n' + name + '\n')
         image_path = path.join(directory, name + '.tif')
         image = load_stab_img(image_path, *stabilize_args)
-        break_y, break_x = np.array(break_y), np.array(break_x)
-        sortind = np.argsort(break_x)
-        for break_centroid in zip(break_y[sortind], break_x[sortind]):
+        for break_centroid in zip(break_y, break_x):
             print('Centroid: ', break_centroid)
             break_image = select_break_image(image, break_centroid)
-            (left_edge_y, left_edge), (right_edge_y, right_edge) = find_gap_edges(break_image)
-            print('Left edge: ', left_edge)
-            print('Right edge: ', right_edge)
-            top_side, bottom_side = find_fiber_sides(break_image, left_edge, right_edge)
-            print('Top side: ', top_side)
-            print('Bottom side: ', bottom_side)
-            gap_width = right_edge - left_edge
-            fiber_diameter = top_side - bottom_side
-            print('Gap width: ', gap_width * PIXEL_SIZE_X, ' um')
-            print('Fiber diameter', fiber_diameter * PIXEL_SIZE_X, ' um')
-            if '0' in name:
-                import matplotlib.pyplot as plt
-                plt.imshow(break_image, cmap='gray')
-                plt.plot([left_edge, right_edge], [left_edge_y, right_edge_y], 'rx')
-                plt.axvline(left_edge, linestyle=':', color='red')
-                plt.axvline(right_edge, linestyle=':', color='red')
-                plt.axhline(top_side, linestyle=':', color='green')
-                plt.axhline(bottom_side, linestyle=':', color='green')
-                plt.show(1)
+            gap_width, fiber_diameter = analyze_break(break_image)
 
 
 if __name__ == '__main__':
-    analyze_breaks(get_folder())
+    folder = (get_folder())
+    analyze_breaks(folder)
+
+    # import cProfile as prof
+    # prof.run('analyze_breaks(folder)',sort='cumtime')
